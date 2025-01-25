410a93910c5d35d439290baaeacd9b822098803b
ShompolovMaxim <89980707+ShompolovMaxim@users.noreply.github.com>
2025-01-26 01:33:00 +0300

    time optimal groupping for l-diversity

--- utility/groupping.py
+++ utility/groupping.py
@@ -1,17 +1,49 @@
-def group_by_dist(df_dists, group_size):
-    grouped = [False] * len(df_dists)
+def group_by_dist(dists, group_size):
+    grouped = [False] * len(dists)
     groups = []
 
     i = 0
-    while i < len(df_dists):
+    while i < len(dists):
         if grouped[i]:
             i += 1
             continue
-        dists = [(df_dists[i][j], j) for j in range(len(df_dists))]
+        dists = [(dists[i][j], j) for j in range(len(dists))]
         dists.sort()
         group = []
         j = 0
-        while len(group) < group_size and j < len(df_dists):
+        while len(group) < group_size and j < len(dists):
+            if not grouped[dists[j][1]]:
+                group.append(dists[j][1])
+                grouped[dists[j][1]] = True
+            j += 1
+        if len(group) < group_size:
+            groups[-1] = groups[-1] + group
+        else:
+            groups.append(group)
+    return groups
+
+def group_by_dist_with_l_diverse(dists, sensitives, group_size, l):
+    grouped = [False] * len(dists)
+    groups = []
+
+    i = 0
+    while i < len(dists):
+        if grouped[i]:
+            i += 1
+            continue
+        dists = [(dists[i][j], j) for j in range(len(dists))]
+        dists.sort()
+        group = []
+        group_sensitives = []
+        j = 0
+        while len(group) < group_size and len(group) < l and j < len(dists):
+            if not grouped[dists[j][1]] and sensitives[j] not in group_sensitives:
+                group.append(dists[j][1])
+                grouped[dists[j][1]] = True
+                group_sensitives.append(sensitives[j])
+            j += 1
+        j = 0
+        while len(group) < group_size and j < len(dists):
             if not grouped[dists[j][1]]:
                 group.append(dists[j][1])
                 grouped[dists[j][1]] = True
